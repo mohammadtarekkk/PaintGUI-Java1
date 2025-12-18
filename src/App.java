7d14d739f992cfc044903d12d9977c8d8a88fe63
asemEmad47 <90583040+asemEmad47@users.noreply.github.com>
2025-12-19 01:54:36 +0200

    Testing the implementation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+
+import ProgramManger.PaintManager;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PaintManager paintManager = new PaintManager();
+        paintManager.runApp();
     }
 }
